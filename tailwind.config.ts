ba1e655d08fc3de13a5730ac32aefef2bff80004
laska-adief <laskaadief.la@gmail.com>
2023-11-16 17:48:48 +0700

    config: tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,28 @@
 import type { Config } from 'tailwindcss'
 
 const config: Config = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+  content: ["./pages/**/*.{js,ts,jsx,tsx,mdx}", "./components/**/*.{js,ts,jsx,tsx,mdx}", "./app/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        normal: "#124A63",
+        secondary: "#B3C5CD",
+        primary: "#21B6B7",
+      },
+      backgroundColor: {
+        normal: "#F9F9F9",
+        dark: "#EEF3F6",
+        light: "#FFFFFF",
+      },
+      fontFamily: {
+        poppins: ["var(--font-poppins), sans-serif"],
       },
     },
   },
   plugins: [],
-}
+};
 export default config
